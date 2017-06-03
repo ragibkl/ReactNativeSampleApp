76b1ec6c7d3d207054963dc6abfe45cfebe60c00
Ragib Badaruddin <ragib.badaruddin@gmail.com>
2017-06-03 15:53:53 +0800

    master - single js entry file - basic loading page - basic app page

---
+++ js/components/app/Loading.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native'
+
+class Loading extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Loading...
+        </Text>
+      </View>
+    )
+  }
+}
+
+export default Loading
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  }
+})
---
+++ js/setup.js
@@ -0,0 +1,7 @@
+import App from './components/app/App'
+import Loading from './components/app/Loading'
+
+export function setup () {
+  // return Loading
+  return App
+}
